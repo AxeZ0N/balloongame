a7bcd9c86057590115c539859482e9752e8b96d7
AxeZ0N <reisbigryan@gmail.com>
2024-11-15 20:27:09 -0500

    foo

--- bubble_game.py
+++ bubble_game.py
@@ -1,31 +1,40 @@
 import sys, pygame
+import random
+
 pygame.init()
 
 size = width, height = 1920, 1080
-speed = [2, 2]
-gray = 100, 100, 100
+speed = [10, 10]
+gray = 0,0,0
 
-screen = pygame.display.set_mode(size)
+running = True
+ball_r = 25
+
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.mouse.set_visible(False)
 
 #ball = pygame.image.load("intro_ball.gif")
 
-
-player_pos = (500,500)
-
-ball = pygame.Surface((10,10), pygame.SRCALPHA)
-pygame.draw.circle(ball, "maroon", (ball.width // 2, ball.height // 2), 10)
+ball = pygame.Surface((ball_r*2, ball_r*2), pygame.SRCALPHA)
+pygame.draw.circle(ball, (255,255,255), (ball.width // 2, ball.height // 2), ball_r)
 ballrect = ball.get_rect()
 
-while True:
+while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            running=False
     pygame.time.delay(10)
 
     ballrect = ballrect.move(speed)
+    s = random.randint(0,1) * 100
+    print(s)
     if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
+        #speed[0] = -speed[0]
+        speed[0] = -s
+    s = random.randint(0,1) * 100
     if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+        #speed[1] = -speed[1]
+        speed[0] = -s
 
     screen.fill(gray)
     screen.blit(ball, ballrect)
