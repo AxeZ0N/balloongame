10b4cea6e01267972691c3c729e26e6c1b227073
AxeZ0N <reisbigryan@gmail.com>
2024-11-24 16:03:36 -0500

    got fingers working

--- touch_test.py
+++ touch_test.py
@@ -1,60 +1,58 @@
 import pygame
 from pygame._sdl2.touch import *
 
-fingers = lambda: [x for x in [get_finger(10,fnum) for fnum in range(5)] if x is not None]
-
-class Main:
-
-    def main(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((720,512))
-        self.bg = pygame.Surface(self.screen.get_size())
-        pygame.mouse.set_visible(False)
-        self.bg.fill("black")
-        self.screen.blit(self.bg, (0,0))
-
-        pygame.display.flip()
-
-        self.my_cokes = [Coke() for _ in range(1)]
-
-        self.all_sprites = pygame.sprite.Group(self.my_cokes[-1])
-        self.clock = pygame.Clock()
-        self.running = True
-
-        self.last_finger = len(fingers())
-
-        while self.running:
-            self.clock.tick(60)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT: self.running = False
-
-                if event.type in (pygame.MOUSEBUTTONDOWN, pygame.MOUSEMOTION):
-                    if (fnum := len(fingers())) != self.last_finger:
-                        self.last_finger = fnum
-                        event.pos = fingers()[-1]
-                    [s.on_click(event) for s in self.all_sprites]
-
-
-        self.screen.blit(bg, (0,0))
-
-        pygame.display.flip()
-
+fingers = lambda: [get_finger(10,fnum) for fnum in range(5)]
 
 class Coke(pygame.sprite.Sprite):
     def __init__(self,*args,**kwargs):
-        self.COKE_RING = 25
-
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.surface.Surface([self.COKE_RING,self.COKE_RING],
-                                            pygame.SRCALPHA)
-        pygame.draw.circle(self.image, "white", 
-                           (self.COKE_RING/2,self.COKE_RING/2), self.COKE_RING/4)
+        self.index = kwargs.get('index',None)
+
+        self.RING = 45
+        self.R = self.RING * 2
+        self.image = pygame.Surface([self.RING * 2, self.RING * 2], pygame.SRCALPHA)
+
+        pygame.draw.circle(self.image, "white", (self.RING, self.RING), self.RING)
 
         self.rect = self.image.get_rect()
 
-    def on_click(self, event):
-        print(event.pos)
+    def update(self):
+        if self.index is not None:
+            f = fingers()[self.index]
+            self.rect.x = f['x'] * 720 - self.RING * 1.4
+            self.rect.y = f['y'] * 512 - self.RING * 1.4 
 
-if __name__=='__main__':
-    Main().main()
-    pygame.quit()
+
+
+pygame.init()
+size = (720,512)
+#screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode(size)
+bg = pygame.Surface(screen.get_size())
+pygame.mouse.set_visible(False)
+bg.fill("black")
+screen.blit(bg, (0,0))
+
+pygame.display.flip()
+
+
+cokes = [Coke() for _ in range(5)]
+all_sprites = pygame.sprite.Group(cokes)
+clock = pygame.Clock()
+running = True
+
+while running:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: running = False
+        if event.type == pygame.MOUSEBUTTONDOWN: all_sprites.add(Coke())
+        if event.type == pygame.MOUSEBUTTONUP: all_sprites.remove(Coke())
+
+    all_sprites.update()
+    update_fingers()
+
+    screen.blit(bg, (0,0))
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+pygame.quit()
