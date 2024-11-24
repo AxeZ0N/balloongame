aabbcbc4382e04504b20cf181cbcf86681a8af6a
AxeZ0N <reisbigryan@gmail.com>
2024-11-24 17:26:36 -0500

    got fingers working

--- touch_test.py
+++ touch_test.py
@@ -1,26 +1,43 @@
 import pygame
+import random
 from pygame._sdl2.touch import *
 
-fingers = lambda: [get_finger(10,fnum) for fnum in range(5)]
+paws = lambda: [get_finger(10,fnum) for fnum in range(5)]
+colors = ['red','orange','yellow','green','blue','indigo','violet']
+print(colors)
 
-class Coke(pygame.sprite.Sprite):
+class Paw(pygame.sprite.Sprite):
     def __init__(self,*args,**kwargs):
         pygame.sprite.Sprite.__init__(self)
         self.index = kwargs.get('index',None)
 
-        self.RING = 45
-        self.R = self.RING * 2
+        self.curr_color = colors.pop(0)
+        colors.append(self.curr_color)
+
+        self.RING = 20
         self.image = pygame.Surface([self.RING * 2, self.RING * 2], pygame.SRCALPHA)
 
-        pygame.draw.circle(self.image, "white", (self.RING, self.RING), self.RING)
+        self.draw_()
 
         self.rect = self.image.get_rect()
 
+    def draw_(self):
+        pygame.draw.circle(self.image, self.curr_color, (self.RING, self.RING), self.RING)
+
+
+    def clean(self):
+        pygame.draw.circle(self.image, pygame.SRCALPHA, (self.RING, self.RING), self.RING)
+
+
     def update(self):
-        if self.index is not None:
-            f = fingers()[self.index]
-            self.rect.x = f['x'] * 720 - self.RING * 1.4
-            self.rect.y = f['y'] * 512 - self.RING * 1.4 
+        try:
+            paw = paws()[self.index]
+            self.draw_()
+            self.rect.x = paw['x'] * size[0] - self.RING * 1.6
+            self.rect.y = paw['y'] * size[1] - self.RING * 2
+
+        except TypeError as e:
+            self.clean()
 
 
 
@@ -36,7 +53,7 @@ screen.blit(bg, (0,0))
 pygame.display.flip()
 
 
-cokes = [Coke() for _ in range(5)]
+cokes = [Paw(index=_) for _ in range(5)]
 all_sprites = pygame.sprite.Group(cokes)
 clock = pygame.Clock()
 running = True
@@ -45,11 +62,10 @@ while running:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: running = False
-        if event.type == pygame.MOUSEBUTTONDOWN: all_sprites.add(Coke())
-        if event.type == pygame.MOUSEBUTTONUP: all_sprites.remove(Coke())
+        if event.type == pygame.MOUSEBUTTONDOWN: all_sprites.add(Paw())
+        if event.type == pygame.MOUSEBUTTONUP: all_sprites.remove(Paw())
 
     all_sprites.update()
-    update_fingers()
 
     screen.blit(bg, (0,0))
     all_sprites.draw(screen)
