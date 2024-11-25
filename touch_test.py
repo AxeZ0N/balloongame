efddae91ee8d4a1b221749eedadff9b2c5582437
AxeZ0N <reisbigryan@gmail.com>
2024-11-24 21:46:52 -0500

    gog

--- touch_test.py
+++ touch_test.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from dataclasses import dataclass
 from pygame._sdl2.touch import *
+from pygame.math import Vector2 as Vec
 
 def paws():
     get_paws = lambda: [get_finger(10,fnum) for fnum in range(5)]
@@ -28,6 +29,8 @@ colors = ['red','orange','yellow','green','blue','indigo','violet']
 class Ball(pygame.sprite.Sprite):
     def __init__(self,*args,**kwargs):
         pygame.sprite.Sprite.__init__(self)
+        self.vel = Vec(5,5)
+        self.move_delta = (self.vel.x,self.vel.y)
 
         self.curr_color = "white"
 
@@ -36,41 +39,65 @@ class Ball(pygame.sprite.Sprite):
         self.image = pygame.Surface([self.BALL * 2, self.BALL * 2], pygame.SRCALPHA)
 
         self.draw_()
-        self.move_delta = (5,5)
 
         self.rect = self.image.get_rect()
         self.area = screen.get_rect()
 
     def draw_(self):
-        pygame.draw.circle(self.image, self.curr_color, (self.BALL, self.BALL), self.BALL)
+        pygame.draw.circle(self.image,
+                           self.curr_color,
+                           (self.BALL,
+                            self.BALL),
+                           self.BALL)
+
+        pygame.draw.rect(self.image,
+                         'black',
+                         pygame.Rect(self.BALL,-self.BALL-3, 3, self.BALL*4))
+
+        pygame.draw.rect(self.image,
+                         'black',
+                         pygame.Rect(-self.BALL-3,self.BALL, self.BALL*4, 3))
 
     def clean(self):
         pygame.draw.circle(self.image, pygame.SRCALPHA, (self.BALL, self.BALL), self.BALL)
 
     def update(self):
         ### bound check bottom ###
-        if self.rect.bottom > self.area.bottom:
-            self.move_delta = (self.move_delta[0], -self.move_delta[1])
-
         ### bound check top ###
-        if self.rect.top < self.area.top:
-            self.move_delta = (self.move_delta[0], -self.move_delta[1])
+        if self.rect.bottom > self.area.bottom or self.rect.top < self.area.top:
+            self.vel = self.vel.reflect(Vec(0,1))
 
         ### bound check left ###
-        if self.rect.left < self.area.left:
-            self.move_delta = (-self.move_delta[0], self.move_delta[1])
-
         ### bound check right ###
-        if self.rect.right > self.area.right:
-            self.move_delta = (-self.move_delta[0], self.move_delta[1])
+        if self.rect.left < self.area.left or self.rect.right > self.area.right:
+            self.vel = self.vel.reflect(Vec(1,0))
+    
+        self.collide()
 
-        self.rect = self.rect.move(self.move_delta)
+        self.rect.x += self.vel.x
+        self.rect.y += self.vel.y
 
-    def collide(self,event):
+    def move_ball(self):
+        self.rect.x += self.vel.x
+        self.rect.y += self.vel.y
+
+    def collide(self,event=False):
         for p in paws():
             if pygame.sprite.collide_circle(p,self):
-                if self.rect.x > event.pos[0] and self.rect.y < event.pos[1]:
-                    self.move_delta = (self.move_delta[0], -self.move_delta[1])
+                if p.rect.x - self.rect.x > 0 and p.rect.y - self.rect.y > 0:
+                    self.vel = self.vel.reflect(Vec(0,0))
+
+                if p.rect.x - self.rect.x > 0 and p.rect.y - self.rect.y < 0:
+                    self.vel = self.vel.reflect(Vec(1,1))
+
+                if p.rect.x - self.rect.x > 0 and p.rect.y - self.rect.y > 0:
+                    self.vel = self.vel.reflect(Vec(1,1))
+
+                if p.rect.x - self.rect.x > 0 and p.rect.y - self.rect.y < 0:
+                    self.vel = self.vel.reflect(Vec(1,1))
+
+                self.rect.x *= 1.1
+                self.rect.y *= 1.1
 
 
 class Paw(pygame.sprite.Sprite):
@@ -122,9 +149,9 @@ class Paw(pygame.sprite.Sprite):
 
 pygame.init()
 size = (720,512)
-#screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size)
 #size = (1920,1080)
-screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+#screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 bg = pygame.Surface(screen.get_size())
 pygame.mouse.set_visible(False)
 bg.fill("black")
@@ -143,11 +170,12 @@ while running:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: running = False
-        if event.type == pygame.MOUSEBUTTONDOWN: all_sprites.add(Paw())
-        if event.type == pygame.MOUSEBUTTONUP: all_sprites.remove(Paw())
-        if event.type == pygame.MOUSEMOTION or \
-                event.type == pygame.MOUSEBUTTONDOWN: 
-                    [p.collide(event) for p in all_sprites]
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            all_sprites.add(Paw())
+        if event.type == pygame.MOUSEBUTTONUP:
+            all_sprites.remove(Paw())
+
+        [s.collide(pygame.mouse.get_pos()) for s in all_sprites]
 
 
     all_sprites.update()
